7c76a36428e9e6c368f916461439d6048136ad48
Dhruv Sood <dhruvsoodboss@gmail.com>
2024-10-13 12:53:15 +0530

    Setup Hero

---
+++ src/components/hero.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Hero() {
+    return (
+        <main>
+            <Spline
+                scene="https://prod.spline.design/GcDV6zFiV2XBYZaj/scene.splinecode"
+            />
+        </main>
+    );
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+
+
+export const Navbar = () => {
+  return (
+    <nav className="w-full flex justify-center items-center text-white absolute h-20 text-2xl">
+        <ul className="flex gap-2">
+            <li>Github</li>
+            <li>LinkedIn</li>
+            <li>Twitter</li>
+            <li>Resume</li>
+        </ul>
+    </nav>
+  )
+}
